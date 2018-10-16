7fc41bec4946cf1118e4f38d15c119dd9043b43d
bevous <43658573+bevous@users.noreply.github.com>
2018-10-15 21:41:33 -0500

    Add files via upload

--- lewis_Nordan_hw4/lewis_Nordan_hw4/wordprocess.cpp
+++ lewis_Nordan_hw4/lewis_Nordan_hw4/wordprocess.cpp
@@ -18,28 +18,74 @@ void wordprocess::setString(string newMessage) {
 }
 
 void wordprocess::wordsInMessage() {
+	words.clear();
 	int y = 0;
-	char buffer[50];
-	for (int x = 0; x < message.length(); x++) {
-		if (message[x] == ' ') {
-			words.emplace_back(message.substr(y,(y-x)));
-			y = x;
-		}
-	}
+	size_t pos = 0;
+	string copymes = message;
+	string delim = " ";
 
+	while ((pos = copymes.find(delim)) != string::npos) {
+		words.push_back( copymes.substr(0, pos) );
+		copymes.erase(0, pos + delim.length());
+	}
+	words.push_back(copymes);
+	/*
+	for (int x = 0; x < words.size(); x++) {
+		cout << words[x] << endl;
+	}*/
 }
 
 string wordprocess::reverse() {
+	wordsInMessage();
 	string currentWord;
-	for (int x = 0; x < words.size(); x++) {
-		currentWord = words[x];
+	//cout << words.size() << endl;
+	for (int x = words.size()-1; x >= 0; x--) {
+		if (x < words.size()-1) {
+			currentWord += " ";
+		}
+		currentWord += words[x];
+		cout << currentWord << "::" << x << endl;
 	}
 	
 	return currentWord;
 }
+
 string wordprocess::sort() {
-	return "help";
+	string sorted;
+	wordsInMessage();
+	string * temp;
+	temp = new string[words.size()];
+
+	for (int x = 0; x<words.size();x++)
+	{
+		temp[x] = words[x];
+		//cout << "word at :" << x << "  " << temp[x] << endl;
+		//cout << "first letter of the word at :" << x << "  " << temp[x][0] << endl;
+	}
+	cout << '\n';
+	for (int y = 0; y < words.size(); y++) {
+	
+		for (int x = 0; x < words.size(); x++) {
+			if (x + 1 < words.size())
+				if (tolower(temp[x][0]) > tolower(temp[x + 1][0])) {
+					//cout << temp[x] << " : " << temp[x + 1] << endl;				
+					temp[x].swap(temp[x + 1]);
+					//cout << temp[x] << " : " << temp[x + 1] << endl;
+				}
+		}
+	
+	}
+	for (int x = 0; x < words.size(); x++) {
+		if (x > 0) {
+			sorted += " ";
+		}
+		sorted += temp[x];
+	}
+
+	delete[] temp;
+	return sorted;
 }
+
 string wordprocess::rot13() {
 	return "help";
 }
